8b3745641928202f998c4c0d6183a8202e109d74
Jeverson misael da cruz filho <jeversonmisaldacruzfilho@gmail.com>
2024-08-01 00:35:17 -0300

    Update cavalos.c

--- cavalos.c
+++ cavalos.c
@@ -4,7 +4,7 @@ int cavalos;
 int resultadoCavalos;
 
 int main() {
-    printf("informe a quantidade de cavalos!\n");
+    printf("informe a quantidade de cavalos, para calcular a quantidade de ferraduras!\n");
     scanf("%d", &cavalos);
     resultadoCavalos= (cavalos*4);
     printf("A quantidade de ferraduras e: %d" , resultadoCavalos);
