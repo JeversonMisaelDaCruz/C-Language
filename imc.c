e543492f87d53baf6df75b6dac05347f148af626
Jeverson misael da cruz filho <jeversonmisaldacruzfilho@gmail.com>
2024-08-21 22:35:30 -0300

    Feat: IMC
    IMC

---
+++ imc.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+  int altura;
+  char sexo;
+
+
+
+
+
+    printf("Informe sua altura\n");
+    scanf("%d", &altura);
+    printf("informe seu sexo\n");
+    scanf("%c", &sexo);
+    if(sexo == 'm'){
+      resultado =
+      
+    }else if(sexo == 'f'){
+      resultado = 
+     
+    }
+    return 0;
+}
+
+
+
+
+// Tendo como dados de entrada a altura e o sexo de uma pessoa, construa um algoritmo que calcule seu peso ideal,
+// utilizando as seguintes fórmulas: ● para homens: (72.7 * h) – 58;
+// ● para mulheres: (62.1 * h) – 44.7.
