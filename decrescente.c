9ee46ab8b4ae0b6029207d67bf1a0ba60fabd877
Jeverson misael da cruz filho <jeversonmisaldacruzfilho@gmail.com>
2024-09-01 16:20:41 -0300

    feat

--- decrescente.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("Hello, World!\n");
-    return 0;
-}
-
-// 6. Escreva um algoritmo que leia três valores inteiros e diferentes e mostre-os em ordem decrescente.
